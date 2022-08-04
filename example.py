a19578702e9443394b633d6b61f95d845f8b05ed
Big watermelon <74262602+Grosse-pasteque@users.noreply.github.com>
2022-08-04 12:26:05 +0200

    Update example.py

--- example.py
+++ example.py
@@ -16,20 +16,3 @@ print(repr(encrypted))
 
 decrypted = chrend.decrypt(encrypted, seed=seed)
 print(repr(decrypted))
-
-
-"""
-If my math doesn't sucks:
-
-should be better than SHA-256:
-
-	2 ** 256  VS  10027035 ** 12
-
-should be better than SHA-512:
-
-	2 ** 256  VS  10027035 ** 23
-
-
-"""
-print('Beat SHA-256:', chrend.combinations(1, 12, 0x110000) > 2 ** 256)
-print('Beat SHA-512:', chrend.combinations(1, 23, 0x110000) > 2 ** 512)
